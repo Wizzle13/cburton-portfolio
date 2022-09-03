aa42a08a364b3cc8652f24b1ed4fe8e45e555714
CBurton <burton.chris13@gmail.com>
2022-09-03 16:41:00 -0500

    cleaned up code and fixed nav layout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,18 +6,18 @@ function Nav(props) {
     return (
         <nav>
             <ul className="flex-row">
-            <li className="mx-2">
-                <Link to="/">About Me</Link>
-            </li>
-            <li className = "mx-2" >
-                <Link to="/portfolio">Portfolio</Link>
-            </li>
-            <li className = "mx-2" >
-                <Link to="/contact">Contact</Link>
-            </li>
-            <li className = "mx-2" >
-                <Link to="/resume">Resume</Link>
-            </li>
+                <li className="mx-2">
+                    <Link to="/">About Me</Link>
+                </li>
+                <li className = "mx-2">
+                    <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li className = "mx-2">
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li className = "mx-2">
+                    <Link to="/resume">Resume</Link>
+                </li>
             </ul>  
         </nav>
     )
