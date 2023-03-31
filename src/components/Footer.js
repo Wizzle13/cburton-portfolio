11ec4df7ba924d08a1f42132c3034032e12c820d
CBurton <burton.chris13@gmail.com>
2023-03-31 09:31:45 -0500

    updated target for footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,12 +7,12 @@ function Footer() {
             <div class="social-container" id="iconLink">
                 <ul className="flex-row">
                     <li>
-                        <a class="btn" href="https://github.com/Wizzle13">
+                        <a class="btn" href="https://github.com/Wizzle13" target="new">
                             <i class="fa fa-github"></i>
                         </a>
                     </li>
                     <li>
-                        <a class="btn" href="https://www.linkedin.com/in/christopher-burton-aba9651b9/">
+                        <a class="btn" href="https://www.linkedin.com/in/christopher-burton-aba9651b9/" target="new">
                             <i class="fa fa-linkedin"></i>
                         </a>
                     </li>
