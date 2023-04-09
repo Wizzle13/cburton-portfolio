7b5b6954bb0b803512802c64261e53c550b5e79f
CBurton <burton.chris13@gmail.com>
2023-04-09 16:22:04 -0500

    added images to resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -11,8 +11,17 @@ function Resume() {
                     <br />
                     <br />
                     <p>
-                        Front-end proficiencies
+                        Front-end Skills
                     </p>
+                    <div>
+                        <img class="skills" src={require(`../assets/images/badges/html.png`)} alt="HTML"/>
+                        <img class="skills" src={require(`../assets/images/badges/CSS.png`)} alt="CSS"/>
+                        <img class="skills" src={require(`../assets/images/badges/JavaScript.png`)} alt="JavaScript"/>
+                        <img class="skills" src={require(`../assets/images/badges/handlebars.png`)} alt="Handlebars"/>
+                        <img class="skills" src={require(`../assets/images/badges/react.png`)} alt="React"/>
+                        <img class="skills" src={require(`../assets/images/badges/bootstrap.png`)} alt="Bootstrap"/>
+                        <img class="skills" src={require(`../assets/images/badges/jquery.png`)} alt="jquery"/>
+                    </div>
                     <ul>
                     
                         <li>React</li>
@@ -24,17 +33,36 @@ function Resume() {
                         <li>Handlebar.js</li>
                     </ul>
                     <p>
-                        Back-end proficiencies
+                        Back-end Skills
                     </p>    
+                    <div>
+                        <img class="skills" src={require(`../assets/images/badges/nodejs.png`)} alt="nodejs"/>
+                        <img class="skills" src={require(`../assets/images/badges/express.png`)} alt="Express"/>
+                        <img class="skills" src={require(`../assets/images/badges/mysql.png`)} alt="MySQL"/>
+                        <img class="skills" src={require(`../assets/images/badges/mongodb.png`)} alt="mongodb"/>
+                    </div>
                     <ul>
                         <li>Node.js</li>
                         <li>Express.js</li>
                         <li>MySQL</li>
                         <li>MongoDB</li>
                     </ul>
+                    <p>
+                        Tools
+                    </p>
+                    <div>
+                        <img class="skills" src={require(`../assets/images/badges/visualstudio.png`)} alt="Visual Studio"/>
+                        <img class="skills" src={require(`../assets/images/badges/github.png`)} alt="GitHub"/>
+                        <img class="skills" src={require(`../assets/images/badges/heroku.png`)} alt="Heroku"/>
+                    </div>
+                    <ul>
+                        <li>Visual Studio</li>
+                        <li>GitHub</li>
+                        <li>Heroku</li>
+                    </ul>
                 </div>
             </section>
-            <section id="badges">
+            <section class="badges">
             <img src={require(`../assets/images/badges/fswd.png`)} alt="Full Stack Web Developer Badge"/>
             </section>
         </div>
